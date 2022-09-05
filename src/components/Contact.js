50ff60ec28a4c7a86ce22fbd6ff6da4dd846309d
Upendrakarri <100344415+Upendrakarri@users.noreply.github.com>
2022-09-05 20:10:03 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,14 @@ const Contact = _ => {
         <section>
             <h2>📞 Contact Me</h2>
             <br/>
-            <a href="https://wa.me/7286888575" target="_blank" rel="noreferrer">What'sApp</a>
+            <a href="https://wa.me/7286888575">WhatsApp</a>
             <br/>
-            <a href="https://github.com/Upendrakarri" target="_blank" rel="noreferrer">Github</a>
+            <a href="https://github.com/Upendrakarri">Github</a>
             <br/>
-            <a href="https://www.linkedin.com/in/upendra-karri-700270225/ target="_blank" rel="noreferrer">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/upendra-karri-700270225/>LinkedIn</a>
             <br/>
-            <a href="https://instagram.com/bunny________135?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">Instagram</a>
+            <a href="https://instagram.com/bunny________135?igshid=YmMyMTA2M2Y=">Instagram</a>
+            <br/>
         </section>
         
     )
